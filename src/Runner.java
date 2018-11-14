ae3683790412fea1adab5742a591842bc559c575
Xander <acox9567@bths.edu>
2018-11-14 10:17:52 -0500

    asdf

--- src/Runner.java
+++ src/Runner.java
@@ -2,25 +2,26 @@
 //Tuesday, November 13, 2018
 
 public class Runner {
-    public void main(String[] args)
+    public static void main(String[] args)
     {
         int[] arr = {0,5,3,7,34,6,7};
         bubbleSort(arr);
         System.out.println(arr);
     }
-    public void bubbleSort(int[] arr)
+    private static void bubbleSort(int[] arr)
     {
         int sorted = arr.length;
         int index = 0;
 
         while (sorted != 0)
         {
-            if (arr[index + 1] < arr[index])
-            {
-                int pHolder = arr[index];
-                arr[index] = arr[index + 1];
-                arr[index + 1] = pHolder;
-                sorted = arr[index + 1];
+            if (index < arr.length - 1) {
+                if (arr[index + 1] < arr[index]) {
+                    int pHolder = arr[index];
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = pHolder;
+                    sorted = arr[index + 1];
+                }
             }
 
             if (index != sorted)
